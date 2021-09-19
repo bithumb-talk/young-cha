9e91c478734a4f3a57c00df50e33d81795928706
jeonjonghyeok <tongher1685@gmail.com>
2021-09-19 18:45:23 +0900

    hotfix: spring RestAPI 제거

--- src/main/java/com/bithumb/crawler/common/response/StatusCode.java
+++
@@ -1,7 +0,0 @@
-package com.bithumb.crawler.common.response;
-
-public enum StatusCode {
-    SUCCESS,
-    FAIL,
-    ERROR
-}
--- src/main/java/com/bithumb/crawler/service/CoinService.java
+++
@@ -1,8 +0,0 @@
-package com.bithumb.crawler.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface CoinService {
-
-}
