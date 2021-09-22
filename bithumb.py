8c065c9feae0db3e25ce31133d32815c77112656
jeonjonghyeok <tongher1685@gmail.com>
2021-09-22 20:05:22 +0900

    fix: .csv file 기능 제거

--- bithumb.py
+++ bithumb.py
@@ -9,7 +9,6 @@ mydb['coins'].drop()
 mycol = mydb['coins']
 
 
-print(my_client.list_database_names())
 url = 'https://www.bithumb.com/'
 req_header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
 res = requests.get(url, headers=req_header)
@@ -19,15 +18,14 @@ if res.ok:
     soup = BeautifulSoup(html, 'html.parser')
     coins = soup.select('#sise_list > tbody > tr')
 
-    with open('bithumb.csv','w',encoding='utf-8') as f:
-        for coin in coins:
-            names = coin.select('td:nth-child(1) > div > p > a > strong')
-            markets = coin.select('td:nth-child(1) > div > p > a > span')
-            for name_obj, market_obj in zip(names,markets):
-                name = re.sub(' 신규 공시', '', name_obj.text.strip())
-                market = market_obj.text.strip().replace('/','_')
-                symbol = market_obj.text.strip().split('/')[0]
-                if market.endswith('KRW'):
-                    f.write(f'{symbol},{market},{name}\n')
-                    mycol.insert_one({"symbol":symbol,"market":market,"korean":name})
+    for coin in coins:
+        names = coin.select('td:nth-child(1) > div > p > a > strong')
+        markets = coin.select('td:nth-child(1) > div > p > a > span')
+        for name_obj, market_obj in zip(names,markets):
+            name = re.sub(' 신규 공시', '', name_obj.text.strip())
+            market = market_obj.text.strip().replace('/','_')
+            symbol = market_obj.text.strip().split('/')[0]
+            if market.endswith('KRW'):
+                mycol.insert_one({"symbol":symbol,"market":market,"korean":name})
+print("Create Coins")
 
