1341e488e91eecaedddf21f997dbd2d76dcaa4f3
jeonjonghyeok <tongher1685@gmail.com>
2021-10-01 22:33:36 +0900

    feat: 유레카 적용

--- src/main/java/com/bithumb/InterestApplication.java
+++ src/main/java/com/bithumb/InterestApplication.java
@@ -2,8 +2,10 @@ package com.bithumb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InterestApplication {
 
 	public static void main(String[] args) {
